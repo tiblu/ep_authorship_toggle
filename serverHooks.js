724495f2f5c1664f9206214e857e1375a70a06ca
Mikk Andresen <mikk.andresen@gmail.com>
2015-12-18 16:38:35 +0200

    Generate author list. The problem tho is that the clientVars.collab_client_vars.historicalAuthorData will not update on the fly when new authors are added after opening the Pad

--- serverHooks.js
+++ serverHooks.js
@@ -16,3 +16,29 @@ exports.padInitToolbar = function (hook_name, args) {
 
     toolbar.registerButton('epAuthorshipToggle', button);
 };
+
+/**
+ * eejsBlock_afterEditbar hook
+ *
+ * Add author list template to the DOM
+ *
+ * @see {@link http://etherpad.org/doc/v1.5.7/#index_eejsblock_name}
+ */
+var eejs = require('ep_etherpad-lite/node/eejs/');
+exports.eejsBlock_afterEditbar = function (hook_name, args, cb) {
+    args.content = args.content + eejs.require("ep_authorship_toggle/templates/authors.ejs");
+    return cb();
+};
+
+/**
+ * eejsBlock_styles hook
+ *
+ * Add plugin stylesheet to the DOM
+ *
+ * @see {@link http://etherpad.org/doc/v1.5.7/#index_eejsblock_name}
+ */
+exports.eejsBlock_styles = function (hook_name, args, cb) {
+    args.content = args.content + eejs.require("ep_authorship_toggle/templates/stylesheets.ejs");
+    return cb();
+};
+
--- static/js/clientHooks.js
+++ static/js/clientHooks.js
@@ -14,10 +14,41 @@
 exports.postToolbarInit = function (hook_name, args) {
     var editbar = args.toolbar; // toolbar is actually editbar - http://etherpad.org/doc/v1.5.7/#index_editbar
 
-    var inner = $('iframe[name=ace_outer]').contents().find('iframe[name=ace_inner]').contents();
-    var docBody = inner.find('#innerdocbody');
+    var $inner = $('iframe[name=ace_outer]').contents().find('iframe[name=ace_inner]').contents();
+    var $docBody = $inner.find('#innerdocbody');
+    var $editorcontainer = $('#editorcontainer');
+
+    var $epAuthorshipToggleAuthorList = $('#epAuthorshipToggleAuthorList');
+    var $epAuthorList = $epAuthorshipToggleAuthorList.find('#authorsList');
 
     editbar.registerCommand('epAuthorshipToggle', function () {
-        docBody.toggleClass('authorColors');
+        var isVisibleAuthor = $epAuthorshipToggleAuthorList.is(':visible');
+
+        $docBody.toggleClass('authorColors');
+
+        if (isVisibleAuthor) { // Is visible so it's gonna be hidden
+            $editorcontainer.css('top', $editorcontainer.offset().top - $epAuthorshipToggleAuthorList.outerHeight());
+            $epAuthorshipToggleAuthorList.toggle();
+        } else {
+            // Clear previous authors and build a new author list
+            $epAuthorList.empty();
+
+            // TODO: Contains only authors that had edited the Pad before the Pad was opened. Will not update on the fly if new authors are added.
+            var authors = clientVars.collab_client_vars.historicalAuthorData;
+            for (var authorId in authors) {
+                if (authors.hasOwnProperty(authorId)) {
+                    var author = authors[authorId];
+                    var elem = $('<span class="author"/>')
+                        .css('background-color', clientVars.colorPalette[author.colorId])
+                        .text(author.name);
+                    $epAuthorList.append(elem);
+                }
+            }
+
+            if (Object.keys(authors).length) {
+                $epAuthorshipToggleAuthorList.toggle(); // Show before to get height
+                $editorcontainer.css('top', $editorcontainer.offset().top + $epAuthorshipToggleAuthorList.outerHeight());
+            }
+        }
     });
 };
